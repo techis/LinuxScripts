2a6d467e74b816a549286d91a54f349e8f738154
techis <techis@gmx.com>
2015-02-23 23:13:49 -0700

    first commit

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Install script for linux scripts
+
+#Install pic.sh and add the alias. Since there is only one script currently this script only installs it. 
+
+echo 'alias scrot="~/pic.sh"' >> ~/.bashrc
+source .bashrc
